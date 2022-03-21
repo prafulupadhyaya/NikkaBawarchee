4eba9afdbfc2781994a77a54f440aeaf54a2bc23
praful upadhyaya <prafulupadhyaya@prafuls-MacBook-Air.local>
2022-03-21 15:22:34 +0530

    second  commit

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner=()=>{
     return <div className="bannerDiv">
         <img src="image.jpeg" className="bannerImage"></img>
-        <h1>NIKKA BAWARCHI</h1>
+        <h1>NIKKA BAWARCHEE</h1>
     </div>;
 }
 export default Banner;
